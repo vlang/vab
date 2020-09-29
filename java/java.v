9981755843a399be795f49b235d1729077d04f8d
lmp <dev.larpon@gmail.com>
2020-09-29 13:34:08 +0200

    More side-by-side fixes

--- java/java.v
+++ java/java.v
@@ -123,7 +123,7 @@ pub fn jdk_found() bool {
 	return jdk_root() != ''
 }
 
-pub fn jdk_bin_path() bool {
+pub fn jdk_bin_path() string {
 	bin_dir := os.find_abs_path_of_executable('javac') or { jdk_root() }
 	return os.dir(bin_dir)
 }
