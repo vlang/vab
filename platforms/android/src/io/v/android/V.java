fae6f4e112aca58383beb0d9867d5cbb12317ef2
lmp <dev.larpon@gmail.com>
2021-01-28 17:35:49 +0100

    java: remove commented imports

--- platforms/android/src/io/v/android/V.java
+++ platforms/android/src/io/v/android/V.java
@@ -2,12 +2,6 @@ package io.v.android;
 
 import android.app.NativeActivity;
 
-/*
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
-*/
-
 public class V extends NativeActivity {
 	static { System.loadLibrary("v"); }
 }
