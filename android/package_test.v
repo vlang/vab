d707962d580adfbdb31519b7fbd6eac43a465ec2
Larpon <Larpon@users.noreply.github.com>
2021-01-29 16:13:35 +0100

    package: use consts in test

--- android/package_test.v
+++ android/package_test.v
@@ -2,8 +2,8 @@
 // Use of this source code is governed by an MIT license file distributed with this software package
 import android
 
-fn test_package_ids() {
-	valid_ids := [
+const (
+	valid_ids   = [
 		'valid.package_id.ok',
 		'your.org.android',
 		'your_.org.android',
@@ -18,17 +18,20 @@ fn test_package_ids() {
 		'your.org_._android',
 		'your.org.android_',
 		'your.org.android32',
-		'your.1.org.11212e'
+		'your.1.org.11212e',
 	]
-	invalid_ids := [
+	invalid_ids = [
 		'_ouch.invalid.package_id',
 		'1.invalid.package_id',
 		'abc',
 		'',
 		'a.b.10',
 		'your.org.11212',
-		'a..b.c'
+		'a..b.c',
 	]
+)
+
+fn test_package_ids() {
 	for id in valid_ids {
 		assert android.is_valid_package_id(id)
 	}
