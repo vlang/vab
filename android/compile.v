b55bf9f3a9e8191d216d2f79931551d247b623bb
lmp <dev.larpon@gmail.com>
2022-05-23 17:35:36 +0200

    ci: debug c flag dumping v17

--- android/compile.v
+++ android/compile.v
@@ -57,7 +57,9 @@ pub fn compile(opt CompileOptions) bool {
 		v_cmd << '-nocache'
 	}
 
-	cross_compiler_name := ndk.compiler(opt.ndk_version, 'armeabi-v7a', opt.api_level) or { '' }
+	cross_compiler_name := ndk.compiler(opt.ndk_version, 'armeabi-v7a', opt.api_level) or {
+		panic('$err_sig: failed getting NDK compiler. $err')
+	}
 	os.setenv('VCROSS_COMPILER_NAME', cross_compiler_name, true)
 	if opt.verbosity > 1 {
 		println('Sat VCROSS_COMPILER_NAME to "$cross_compiler_name"')
