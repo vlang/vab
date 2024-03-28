a628e95b217a5593f354e45aac625e859a6908d0
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-03-28 15:28:11 +0100

    andorid: add cflag to stbi import

--- android/compile.v
+++ android/compile.v
@@ -688,6 +688,7 @@ pub fn compile_v_imports_c_dependencies(opt CompileOptions, imported_modules []s
 			build_cmd := [
 				compiler,
 				cflags.join(' '),
+				'-mfpu=neon-vfpv4',
 				'-Wno-sign-compare',
 				'-I"' + os.join_path(v_thirdparty_dir, 'stb_image') + '"',
 				'-c "' + os.join_path(v_thirdparty_dir, 'stb_image', 'stbi.c') + '"',
