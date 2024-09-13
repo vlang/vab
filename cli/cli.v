57f25c00ba090e9dea87162d0ac9ec0acd8cbce1
lmp <dev.larpon@gmail.com>
2024-09-13 22:11:40 +0200

    cli, tests: fix `input_from_args`, fix cli tests

--- cli/cli.v
+++ cli/cli.v
@@ -86,6 +86,8 @@ pub fn input_from_args(arguments []string) (string, []string) {
 	if unmatched.len > 0 && (os.is_dir(unmatched[0]) || os.is_file(unmatched[0])) {
 		input = unmatched[0]
 		args.delete(args.index(input))
+	} else if opts.input != '' {
+		args.delete(args.index(opts.input))
 	}
 	return input, args
 }
