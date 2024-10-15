7e03028707aa9a6464e9fec37f1038e52668ad69
lmp <dev.larpon@gmail.com>
2024-10-15 19:32:07 +0200

    fix

--- cli/remedy.v
+++ cli/remedy.v
@@ -13,7 +13,7 @@ pub fn notify_known_errors(opt Options) {
 				symptoms_apk := "\taapt E 10-15 18:51:31 89712 89712] Entry offset at index 1335 points outside the Type's boundaries
 \t.../res/values/styles.xml:4: error: Error retrieving parent for item: No resource found that matches the given name 'android:Theme.Holo.Light.DarkActionBar'."
 				symptoms := if opt.package_format == 'aab' { symptoms_aab } else { symptoms_apk }
-				util.vab_notice('Using build-tools < 35.0.0 with Android API level >= 33 is know to cause package build errors',
+				util.vab_notice('Using build-tools < 35.0.0 with Android API level >= 33 is known to cause package build errors',
 					details: 'Symptoms output:\n${symptoms}\nIt can usually be fixed by installing and using build-tools >= 35.0.0\nTry:\n\tvab install "build-tools;35.0.0"'
 				)
 			}
